8afe830371b8993839455494e365f3c80516c39b
Adarsh-aot <160816474+Adarsh-aot@users.noreply.github.com>
2024-06-20 05:31:02 -0700

    Assignment

---
+++ script.js
@@ -0,0 +1,193 @@
+let mydata = JSON.parse(localStorage.getItem('todos')) || [];
+const todoList = document.getElementById('todo');
+const completedtodoList = document.getElementById('completed-todo');
+const update = document.getElementById('update');
+// Function to add a new task
+function addTask() {
+    const name = document.getElementById('name').value;
+    const description = document.querySelector('.discription').value;
+    const date = document.getElementById('date').value;
+    const completed = false;
+    if (name && description && date) {
+        const obj = {
+            name,
+            description,
+            date,
+            completed
+        };
+        mydata.push(obj);
+        localStorage.setItem('todos', JSON.stringify(mydata));
+        window.location.reload();
+    } else {
+        alert('Please fill out all fields');
+    }
+}
+
+// Function to render the todos (Optional, if you want to display tasks)
+function renderTodos() {
+    todoList.innerHTML = ' ';
+    completedtodoList.innerHTML = '';
+    mydata.forEach((todo, index) => {
+        if(todo.completed == false){ 
+        const div = document.createElement('div');
+        div.className = 'todo-item';
+        div.innerHTML = `
+            
+            
+            
+            <div class="todo-content">
+                <input type="radio" name="completed" id="" class="completed" onclick="complete(${index})"> 
+                 <div class="content">
+                    <div class="color-row"><h1>${todo.name} </h1><p class="color"></p></div>
+                    <p>d${todo.description} </p>
+                    <div class="calander">
+                    <img src="./Pictures/calendar_month_black_24dp 2.svg" alt="">
+                    <p>${todo.date}</p>
+                </div>
+                
+            </div>
+            <div>
+                <button class="edit-btn" ><img src="./Pictures/Group 817.svg" alt="" data-toggle="modal" data-target="#myModaledit" onclick="update_content(${index})"></button>
+            <button class="delete-btn" onclick="deleteTodo(${index})"><img src="./Pictures/Group.svg" alt=""></button>
+            </div>
+
+            
+            
+        `;
+        todoList.appendChild(div);
+        }
+        else{
+            const div = document.createElement('div');
+        div.className = 'todo-item';
+        div.innerHTML = `
+            
+            
+            
+            <div class="todo-content">
+                <input type="radio" name="completed" id="" class="completed" onclick="complete(${index})"> 
+                 <div class="content">
+                    <div class="color-row"><h1>${todo.name} </h1><p class="completed-color"></p></div>
+                    <p>d${todo.description} </p>
+                    <div class="calander">
+                    <img src="./Pictures/calendar_month_black_24dp 2.svg" alt="">
+                    <p>${todo.date}</p>
+                </div>
+                
+            </div>
+            <div>
+                <button class="edit-btn" ><img src="./Pictures/Group 817.svg" alt="" data-toggle="modal" data-target="#myModaledit" onclick="update_content(${index})"></button>
+            <button class="delete-btn" onclick="deleteTodo(${index})"><img src="./Pictures/Group.svg" alt=""></button>
+            </div>
+            
+            
+            
+        `;
+        completedtodoList.appendChild(div);
+        }
+    });
+}
+
+function complete(index){
+    let storedArray = JSON.parse(localStorage.getItem('todos'));
+    let updated = storedArray[index];
+    storedArray[index].completed = true ;
+    localStorage.setItem('todos', JSON.stringify(storedArray));
+    window.location.reload();
+    console.log("complete")
+}
+
+function update_content(index){
+    console.log("rf")
+    let storedArray = JSON.parse(localStorage.getItem('todos'));
+    let updated = storedArray[index];
+    console.log(updated)
+    update.innerHTML = `
+    <div class="modal" id="myModaledit">
+          <div class="modal-dialog">
+            <div class="modal-content">
+            
+              <!-- Modal Header -->
+              <div class="modal-header">
+                <h4 class="modal-title">Add Task</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+              </div>
+              
+              <!-- Modal body -->
+              <div class="modal-body">
+                <div class="label">
+                Title *
+                <input type="text" id="update_name" class="name" value="${updated.name}">
+                </div>
+                <div class="label"><p>Description <img src="./Pictures/Vector.svg" alt=""></p>
+                <input type="text" class="discription" id="update_discription" value="${updated.description}">
+                
+                </div>
+                <div class="label">Due Date
+                <input type="date" name="date"   id="update_date" value="${updated.date}">
+                </div>
+                
+              </div>
+              
+              <!-- Modal footer -->
+              <div class="modal-footer">
+                <button type="button" class="btn cancel-task-btn" class="close" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary add-task-btn add-btn" onclick="updateTodo(${updated.completed} ,${index} )">Add Task</button>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    `
+    
+}
+
+
+function clearAllcompleted(){
+    mydata = mydata.filter((todo) => todo.completed == false);
+    updateLocalStorage();
+    renderTodos();
+}
+
+
+
+
+
+function updateTodo(completed_status , index  ) {
+    const name = document.getElementById('update_name').value;
+    const description = document.getElementById('update_discription').value;
+    const date = document.getElementById('update_date').value;
+    const completed = completed_status;
+    console.log(name , description , date)
+    if (name && description && date) {
+        const obj = {
+            name : name,
+            description : description,
+            date : date,
+            completed : completed
+        };
+        let storedArray = JSON.parse(localStorage.getItem('todos'));
+        let updated = storedArray[index];
+        storedArray[index] = obj ;
+        localStorage.setItem('todos', JSON.stringify(storedArray));
+        window.location.reload();
+    } else {
+        console.log(name , description , date)
+        alert('Please fill out all fields');
+    }
+}
+
+
+function deleteTodo(index) {
+    mydata.splice(index, 1);
+    updateLocalStorage();
+    renderTodos();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(mydata));
+}
+
+// Call renderTodos on page load (Optional)
+window.onload = renderTodos;
